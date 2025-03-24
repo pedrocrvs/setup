81100477d3949eb87d46be605b3ddb5da51c0939
Pedro Cardo Vasconcellos <pedro.cardo.vasconcellos@gmail.com>
2025-03-23 23:42:21 -0300

    (fish) modifica a variável de ambiente `LS_COLORS`

--- resources/fish/conf.d/ls_colors.fish
+++ resources/fish/conf.d/ls_colors.fish
@@ -1,3 +1,3 @@
 if status is-interactive
-    set --global --export LS_COLORS "bd=97:cd=97:di=2;37:ex=97:fi=97:ln=4;97:mi=4;91:or=4;91:ow=97:pi=97:sg=97:so=97:st=97:su=97:tw=97"
+    set --global --export LS_COLORS "bd=97:cd=97:di=4;97:ex=97:fi=97:ln=4;97:mi=4;91:or=4;91:ow=97:pi=97:sg=97:so=97:st=97:su=97:tw=97"
 end
