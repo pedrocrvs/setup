b2e8a3eb5fea61d403a19d3cb7054b84240d567b
Pedro Cardo Vasconcellos <pedro.cardo.vasconcellos@gmail.com>
2025-03-23 23:41:40 -0300

    (pwsh) modifica a variável de ambiente `LS_COLORS`

--- resources/powershell/profile.ps1
+++ resources/powershell/profile.ps1
@@ -257,7 +257,7 @@ else {
 }
 
 
-$Env:LS_COLORS = "bd=97:cd=97:di=2;37:ex=97:fi=97:ln=4;97:mi=4;91:or=4;91:ow=97:pi=97:sg=97:so=97:st=97:su=97:tw=97"
+$Env:LS_COLORS = "bd=97:cd=97:di=4;97:ex=97:fi=97:ln=4;97:mi=4;91:or=4;91:ow=97:pi=97:sg=97:so=97:st=97:su=97:tw=97"
 
 $Env:STARSHIP_CACHE = "$Env:APPDATA\starship\"
 
