d4068af3071672b1178bc8cc6aa140385d9f9007
Pedro Cardo Vasconcellos <pedro.cardo.vasconcellos@gmail.com>
2025-09-15 04:00:48 -0300

    (lesskey) adiciona atalhos

---
+++ scripts/debian/configure-lesskey.ps1
@@ -0,0 +1,6 @@
+$ErrorActionPreference = "Stop"
+
+
+$ResourceConfigFile = wsl --distribution "Debian" --exec "wslpath" "$PSScriptRoot\..\..\resources\lesskey\.lesskey"
+
+wsl --distribution "Debian" --exec "cp" "--no-preserve=mode" $ResourceConfigFile "/home/pedro/"
