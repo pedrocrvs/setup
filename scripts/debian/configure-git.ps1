1fe22528392c5ddbc551f6ffae0f00f3e598e921
Pedro Cardo Vasconcellos <pedro.cardo.vasconcellos@gmail.com>
2025-12-03 23:28:10 -0300

    reformata Scripts

--- scripts/debian/configure-git.ps1
+++ scripts/debian/configure-git.ps1
@@ -3,4 +3,5 @@ $ErrorActionPreference = "Stop"
 
 $ResourceConfigFile = wsl --distribution "Debian" --exec "wslpath" "$PSScriptRoot\..\..\resources\git\.gitconfig"
 
+
 wsl --distribution "Debian" --exec "cp" "--no-preserve=mode" $ResourceConfigFile "/home/pedro/"
--- scripts/debian/configure-nushell.ps1
+++ scripts/debian/configure-nushell.ps1
@@ -3,6 +3,7 @@ $ErrorActionPreference = "Stop"
 
 $ResourceConfigDirectory = wsl --distribution "Debian" --exec "wslpath" "$PSScriptRoot\..\..\resources\nushell\"
 
+
 wsl --distribution "Debian" --exec "mkdir" "/home/pedro/.config/" 2> $null
 
 wsl --distribution "Debian" --exec "cp" "--no-preserve=mode" "--recursive" $ResourceConfigDirectory "/home/pedro/.config/"
